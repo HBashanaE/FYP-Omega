d3198fb9c1292801fec82c004992a0050122d618
Yasith-Banuka <Yasith-Banuka@users.noreply.github.com>
2021-10-21 16:15:44 +0530

    suggestion generator bug fix

--- Spell checker/suggestionGenerator.py
+++ Spell checker/suggestionGenerator.py
@@ -12,19 +12,22 @@ class SuggestionGenerator():
         padded = error[:-1]
         padded.insert(0,'<s>')
         for idx, letter in enumerate(padded):
-            for correction in self.insertions[letter]:
-                suggestion = error[:idx] + [correction[0]] + error[idx:]
-                suggestions.append((suggestion,correction[1]))
+            if letter in self.insertions:
+                for correction in self.insertions[letter]:
+                    suggestion = error[:idx] + [correction[0]] + error[idx:]
+                    suggestions.append((suggestion,correction[1]))
         
         for idx, letter in enumerate(padded):
-            for correction in self.deletions[letter]:
-                suggestion = error[:idx] + error[idx:]
-                suggestions.append((suggestion,correction[1]))
+            if letter in self.deletions:
+                for correction in self.deletions[letter]:
+                    suggestion = error[:idx] + error[idx:]
+                    suggestions.append((suggestion,correction[1]))
 
         
         for idx, letter in enumerate(error):
-            for correction in self.substitutions[letter]:
-                suggestion = error[:idx] + [correction[0]] + error[idx+1:]
-                suggestions.append((suggestion,correction[1]))   
+            if letter in self.substitutions:
+                for correction in self.substitutions[letter]:
+                    suggestion = error[:idx] + [correction[0]] + error[idx+1:]
+                    suggestions.append((suggestion,correction[1]))   
 
         return suggestions            
