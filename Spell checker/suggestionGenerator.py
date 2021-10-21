3e4fc201f73c71a37bfd172e31db2a9229767c55
Yasith-Banuka <Yasith-Banuka@users.noreply.github.com>
2021-10-21 21:18:12 +0530

    "sugestion tokenizing

--- Spell checker/suggestionGenerator.py
+++ Spell checker/suggestionGenerator.py
@@ -1,3 +1,6 @@
+from unicodedata import normalize as unicodeNormalize
+import regex as re
+
 class SuggestionGenerator():
 
     def __init__(self, insertions, deletions, substitutions, baseProbability) -> None:
@@ -8,26 +11,72 @@ class SuggestionGenerator():
 
     #error -> (list) tokenized error word
     def generateSuggestions(self, error):
-        suggestions = [(error,self.baseProbability)]
-        padded = error[:-1]
+        tokenizedError = self.tokenize(error)
+        suggestions = [(tokenizedError,self.baseProbability)]
+        padded = tokenizedError[:-1]
         padded.insert(0,'<s>')
         for idx, letter in enumerate(padded):
             if letter in self.insertions:
                 for correction in self.insertions[letter]:
-                    suggestion = error[:idx] + [correction[0]] + error[idx:]
+                    suggestion = tokenizedError[:idx] + [correction[0]] + tokenizedError[idx:]
                     suggestions.append((suggestion,correction[1]))
         
         for idx, letter in enumerate(padded):
             if letter in self.deletions:
                 for correction in self.deletions[letter]:
-                    suggestion = error[:idx] + error[idx:]
+                    suggestion = tokenizedError[:idx] + tokenizedError[idx:]
                     suggestions.append((suggestion,correction[1]))
 
         
-        for idx, letter in enumerate(error):
+        for idx, letter in enumerate(tokenizedError):
             if letter in self.substitutions:
                 for correction in self.substitutions[letter]:
-                    suggestion = error[:idx] + [correction[0]] + error[idx+1:]
+                    suggestion = tokenizedError[:idx] + [correction[0]] + tokenizedError[idx+1:]
                     suggestions.append((suggestion,correction[1]))   
 
-        return suggestions            
+        return suggestions
+
+    
+
+
+
+    def tokenize(text):
+        suffixesList = [ 
+        "්", 
+        "ා", 
+        "ැ", 
+        "ෑ", 
+        "ි", 
+        "ී", 
+        "ු", 
+        "ූ", 
+        "ෙ", 
+        "ේ", 
+        "ෛ", 
+        "ො", 
+        "ෝ" ,
+        "ෞ",
+        "ෘ",
+        "ෲ"
+        ]
+        tokens = []
+        li = 1
+        while li < len(text):
+        # for li in range(1, len(text)):
+            prevChar, currentChar = text[li - 1], text[li]
+            if(currentChar == '\u200d'):
+                    if(li < len(text) - 1):
+                        if(prevChar == suffixesList[0] and (text[li + 1] == 'ර' or text[li + 1] == 'ය' or text[li + 1] == 'ද')):
+                            tokens.append(tokens.pop()+currentChar+text[li + 1])
+                            li += 1
+            elif(currentChar in suffixesList):
+                if(li != 1):
+                    tokens.append(f"{tokens.pop()}{currentChar}")
+                else:
+                    tokens.append(f"{prevChar}{currentChar}")
+            else:
+                if(li == 1):
+                    tokens.append(prevChar)
+                tokens.append(currentChar)
+            li += 1
+        return tokens            
