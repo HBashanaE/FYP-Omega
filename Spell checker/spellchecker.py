d15165667a84c41a3b430d4c331b1116b0a98670
Yasith-Banuka <Yasith-Banuka@users.noreply.github.com>
2021-10-21 17:54:49 +0530

    spell checker changes. add paths

--- Spell checker/spellchecker.py
+++ Spell checker/spellchecker.py
@@ -5,10 +5,16 @@ import json
 
 class SpellChecker():
 
-    def __init__(self, dictionaryPath, neuralModelpath, insertions, deletions, substitutions) -> None:
+    def __init__(self, dictionaryPath, neuralModelpath, insertionPath, deletionPath, substitutionPath) -> None:
         with open(dictionaryPath, 'r', encoding='utf-8') as json_file:
             dictionary = json.load(json_file)
         self.evaluationModule = EvaluationMmodule(dictionary, neuralModelpath)
+        with open(insertionPath, 'r', encoding='utf-8') as json_file:
+            insertions = json.load(json_file)
+        with open(deletionPath, 'r', encoding='utf-8') as json_file:
+            deletions = json.load(json_file)
+        with open(substitutionPath, 'r', encoding='utf-8') as json_file:
+            substitutions = json.load(json_file)
         self.suggestionGenerator = SuggestionGenerator(insertions, deletions, substitutions)
 
     def correctSpelling(self, errorName):
