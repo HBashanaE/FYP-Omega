9737cc76c00f589f64703ab72a85505e0fd86aa8
Yasith-Banuka <47107056+Yasith-Banuka@users.noreply.github.com>
2022-04-05 10:32:33 +0530

    Update suggestionGenerator.py

--- Spell_Checker/suggestionGenerator.py
+++ Spell_Checker/suggestionGenerator.py
@@ -18,6 +18,7 @@ class SuggestionGenerator():
     def generateSuggestions(self, error):
         tokenizedError = tokenize_full(error)
         suggestions = [(tokenizedError, self.baseProbability)]
+        sug_list = [tokenizedError]
         padded = tokenizedError[:-1]
         padded.insert(0, '<s>')
         for idx, letter in enumerate(padded[1:]):
@@ -26,27 +27,35 @@ class SuggestionGenerator():
                     if correction[0] == padded[idx-1]:
                         suggestion = tokenizedError[:idx] + \
                             tokenizedError[idx+1:]
-                        suggestions.append((suggestion, correction[1]))
+                        if suggestion not in sug_list:
+                            suggestions.append((suggestion, correction[1]))
+                            sug_list.append(suggestion)
 
         for idx, letter in enumerate(padded):
             if letter in self.deletions:
                 for correction in self.deletions[letter]:
                     suggestion = tokenizedError[:idx] + \
                         [correction[0]] + tokenizedError[idx:]
-                    suggestions.append((suggestion, correction[1]))
+                        if suggestion not in sug_list:
+                            suggestions.append((suggestion, correction[1]))
+                            sug_list.append(suggestion)
 
         for idx, letter in enumerate(tokenizedError):
             if letter in self.substitutions:
                 for correction in self.substitutions[letter]:
                     suggestion = tokenizedError[:idx] + \
                         [correction[0]] + tokenizedError[idx+1:]
-                    suggestions.append((suggestion, correction[1]))
+                        if suggestion not in sug_list:
+                            suggestions.append((suggestion, correction[1]))
+                            sug_list.append(suggestion)
 
         max_score = max(suggestions, key=itemgetter(1))[1]
-        icm_suggestions = [sug[0] for sug in suggestions]
 
         for x, y in self.dictionary.items():
-            if (0 < Levenshtein.distance(x, error) < 3) and (x not in icm_suggestions):
-                suggestions.append((tokenize_full(x), max_score*y))
+            if (0 < Levenshtein.distance(x, error) < 3) and (x not in sug_list):
+                suggestion = tokenize_full(x)
+                suggestions.append((suggestion, max_score*y))
+                sug_list.append(suggestion)
+                
 
-        return sorted(suggestions, key=lambda tup: tup[1], reverse=True)
+        return suggestions
